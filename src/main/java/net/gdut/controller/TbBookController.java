e5949fd325b84d4b897c03d33574450db60a4c5a
ue羿 <626422973@qq.com>
2020-07-28 20:10:03 +0800

    管理书籍
    最后post失败

--- src/main/java/net/gdut/controller/TbBookController.java
+++ src/main/java/net/gdut/controller/TbBookController.java
@@ -1,7 +1,10 @@
 package net.gdut.controller;
 
+import net.gdut.bean.TbBook;
 import net.gdut.service.TbBookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.Resource;
 
@@ -10,5 +13,17 @@ public class TbBookController {
     @Resource
     TbBookService tbBookService;
 
+    @PostMapping("/addBook")
+    public String addBook(TbBook TbBook){
+    tbBookService.addBook(TbBook);
+        return "admin"; }
+
+    @PostMapping("/deleteBook")
+    public String deleteBook(TbBook TbBook){
+        tbBookService.deleteBook(TbBook);
+        return "admin"; }
+
+    @GetMapping("/admin")
+    public String admin(){ return "admin"; }
 
 }
