9e70a31d4293a6fa11595c578f8cb436253bfb41
ue羿 <626422973@qq.com>
2020-07-28 16:51:58 +0800

    增删书籍半成品
    111

--- src/main/java/net/gdut/service/impl/TbBookServiceImpl.java
+++ src/main/java/net/gdut/service/impl/TbBookServiceImpl.java
@@ -1,4 +1,21 @@
 package net.gdut.service.impl;
 
-public class TbBookServiceImpl {
+import net.gdut.bean.TbBook;
+import net.gdut.mapper.TbBookMapper;
+import net.gdut.service.TbBookService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class TbBookServiceImpl implements TbBookService {
+    @Resource
+    TbBookMapper tbBookMapper;
+    @Override
+    public void addBook(TbBook tbBook) {
+        tbBookMapper.addBook(tbBook);
+    }
+    @Override
+    public void deleteBook(TbBook tbBook) { tbBookMapper.deleteBook(tbBook); }
+
 }
