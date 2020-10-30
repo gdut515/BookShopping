5de3ac4526d269410314048999a2710a16251ab6
Aery <KillerAery@foxmail.com>
2020-10-30 18:39:10 +0800

    移出不必要User服务接口

--- src/main/java/net/gdut/mapper/UserMapper.java
+++ src/main/java/net/gdut/mapper/UserMapper.java
@@ -12,8 +12,6 @@ import java.util.List;
 public interface UserMapper {
     //添加普通用户
     public void addUser(User user);
-    //添加管理员用户
-    public void addAdmin(User user);
     //查询用户
     public User getUser(String uname);
     //查询用户权限
